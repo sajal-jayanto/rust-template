62cc8e2bd9d50ae37f904584c821a54f32c4ab26
sajal-jayanto <sajaljayanto007@gmail.com>
2026-08-17 09:44:48 +0600

    fix code formate

--- src/routes/v1/sample.rs
+++ src/routes/v1/sample.rs
@@ -1,14 +1,19 @@
 use axum::{Router, routing::{delete, get, post, put}};
 
 use crate::services::sample::{
-  create_sample, delete_sample, get_all_sample, get_sample_by_id, swap_sample_names, update_sample,
+  create_sample, 
+  delete_sample, 
+  get_all_sample, 
+  get_sample_by_id, 
+  swap_sample_names,
+  update_sample,
 };
 
 pub fn routes() -> Router {
   Router::new()
     .route("/", get(get_all_sample))
-    .route("/{id}", get(get_sample_by_id))
     .route("/", post(create_sample))
+    .route("/{id}", get(get_sample_by_id))
     .route("/{id}", put(update_sample))
     .route("/{id}", delete(delete_sample))
     .route("/swap", post(swap_sample_names))
